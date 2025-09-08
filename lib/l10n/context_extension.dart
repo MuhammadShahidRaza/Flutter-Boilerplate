5525ed55b235e6edb8302a0f099613e8567a1e06
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-09 01:27:05 +0500

    extention added

--- lib/l10n/context_extension.dart
+++ lib/l10n/context_extension.dart
@@ -2,9 +2,11 @@ import 'package:flutter/widgets.dart';
 import 'app_localizations.dart';
 
 extension TranslateX on BuildContext {
-  String tr(String key) {
-    return AppLocalizations.of(this)!.translate(key);
+  String tr(String key, {Map<String, String>? params}) {
+    return AppLocalizations.of(this)!.translate(key, params: params);
   }
 }
 
-// Text(context.tr("hello"))
+// Text(
+//   context.tr("GREETING", params: {"name": "Shahid"}),
+// ),
