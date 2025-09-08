355b0a6874cad4dbe2a19473aebda44c8ce5a28c
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-09 01:28:09 +0500

    colors added

---
+++ lib/theme/colors.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // App Specific
+  static const Color primary = Color(0xFF051229);
+  static const Color secondary = Color(0xFF0AC8FF);
+  static const Color error = Color(0xFFFB344F);
+  static const Color backgroundColor = Colors.white;
+  static const Color scaffoldBackgroundColor = Color(0xFFF5F5F5);
+  static const Color icons = Color(0xFF676767);
+  static const Color header = Color(0xFFECECEC);
+  static const Color inputBackground = Color(0xFFF9F9F9);
+  static const Color border = Color(0xFFCACACA);
+  static const Color text = Color(0xFF4D4D4D);
+  static const Color greenStatus = Color(0xFF00B406);
+
+  // Common
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightWhite = Color(0xFFFAFBFF);
+  static const Color black = Color(0xFF000000);
+  static const Color blue = Color(0xFF0000FF);
+  static const Color darkBlue = Color(0xFF173C47);
+  static const Color lightGrey = Color(0xFFD9D9D9);
+  static const Color searchBar = Color(0xFFF4F5F6);
+  static const Color darkGrey = Color(0xFF858585);
+  static const Color mediumGrey = Color(0xFF343434);
+  static const Color purple = Color(0xFF800080);
+  static const Color red = Color(0xFFFF0000);
+  static const Color green = Color(0xFF008000);
+  static const Color yellow = Color(0xFFFFFF00);
+  static const Color orange = Color(0xFFFFA500);
+  static const Color pink = Color(0xFFFFC0CB);
+  static const Color cyan = Color(0xFF00FFFF);
+  static const Color magenta = Color(0xFFFF00FF);
+  static const Color lime = Color(0xFF00FF00);
+  static const Color teal = Color(0xFF008080);
+  static const Color indigo = Color(0xFF4B0082);
+  static const Color brown = Color(0xFFA52A2A);
+  static const Color silver = Color(0xFFC0C0C0);
+  static const Color gold = Color(0xFFFFD700);
+  static const Color maroon = Color(0xFF800000);
+  static const Color navy = Color(0xFF000080);
+  static const Color olive = Color(0xFF808000);
+  static const Color gray = Color(0xFF808080);
+  static const Color skyBlue = Color(0xFF87CEEB);
+  static const Color coral = Color(0xFFFF7F50);
+  static const Color lavender = Color(0xFFE6E6FA);
+  static const Color turquoise = Color(0xFF40E0D0);
+  static const Color salmon = Color(0xFFFA8072);
+  static const Color crimson = Color(0xFFDC143C);
+  static const Color darkGreen = Color(0xFF006400);
+  static const Color darkOrange = Color(0xFFFF8C00);
+  static const Color darkRed = Color(0xFF8B0000);
+  static const Color darkViolet = Color(0xFF9400D3);
+  static const Color fireBrick = Color(0xFFB22222);
+  static const Color forestGreen = Color(0xFF228B22);
+  static const Color darkSlateGray = Color(0xFF2F4F4F);
+  static const Color lightCoral = Color(0xFFF08080);
+  static const Color lightSeaGreen = Color(0xFF20B2AA);
+  static const Color mediumBlue = Color(0xFF0000CD);
+  static const Color mediumVioletRed = Color(0xFFC71585);
+  static const Color midnightBlue = Color(0xFF191970);
+  static const Color peru = Color(0xFFCD853F);
+  static const Color rosyBrown = Color(0xFFBC8F8F);
+  static const Color sienna = Color(0xFFA0522D);
+  static const Color slateBlue = Color(0xFF6A5ACD);
+  static const Color tomato = Color(0xFFFF6347);
+  static const Color steelBlue = Color(0xFF4682B4);
+
+  // Transparency / Opacity
+  static const Color transparent = Colors.transparent;
+  static const Color darkBlackOpacity = Color.fromRGBO(2, 2, 2, 0.8);
+  static const Color mediumBlackOpacity = Color.fromRGBO(2, 2, 2, 0.6);
+  static const Color whiteOpacity = Color.fromRGBO(255, 255, 255, 0.4);
+}
