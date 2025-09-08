efa9dba7e0e45c7e75f8e822fc9572b7981b28b2
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-09 01:27:23 +0500

    app routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  // Auth Navigation route names
+  static const String login = '/login';
+
+  // App Navigation route names
+  static const String home = '/';
+  static const String homes = '/ghvj';
+}
