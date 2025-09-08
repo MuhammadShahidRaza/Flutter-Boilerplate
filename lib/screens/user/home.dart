2507edaa427563ac2b65365d77acaace6279e559
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-09 01:27:48 +0500

    home

---
+++ lib/screens/user/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:sanam_laundry/constants/strings.dart';
+import 'package:sanam_laundry/l10n/context_extension.dart';
+import 'package:sanam_laundry/routes/app_routes.dart';
+import 'package:sanam_laundry/routes/navigator.dart';
+import 'package:sanam_laundry/services/auth.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: ElevatedButton(
+          child: Text(context.tr(Common.logout)),
+          onPressed: () {
+            AuthService.removeToken();
+            context.replacePage(AppRoutes.login);
+          },
+        ),
+      ),
+    );
+  }
+}
