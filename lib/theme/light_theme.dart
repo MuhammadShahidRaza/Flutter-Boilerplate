b4670fa55bc0ecd4128d00a959e0106c68bbfd38
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-10 01:15:09 +0500

    light theme

---
+++ lib/theme/light_theme.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:sanam_laundry/theme/colors.dart';
+import 'package:sanam_laundry/theme/text.dart';
+
+class LightTheme {
+  static ThemeData get theme => ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+      primary: AppColors.primary,
+      onPrimary: Colors.white,
+      secondary: AppColors.secondary,
+      onSecondary: Colors.white,
+      surface: AppColors.scaffoldBackground,
+      onSurface: AppColors.text,
+      error: AppColors.error,
+      onError: Colors.white,
+    ),
+    scaffoldBackgroundColor: AppColors.scaffoldBackground,
+    textTheme: CustomTextTheme.getTextTheme(
+      AppColors.text,
+      AppColors.secondary,
+    ),
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      backgroundColor: AppColors.primary,
+      foregroundColor: Colors.white,
+    ),
+    iconTheme: const IconThemeData(color: AppColors.icons),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        minimumSize: WidgetStatePropertyAll(const Size.fromHeight(48)),
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+        backgroundColor: WidgetStatePropertyAll(AppColors.primary),
+        foregroundColor: WidgetStatePropertyAll(Colors.white),
+      ),
+    ),
+
+    // Outlined Button
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: ButtonStyle(
+        minimumSize: WidgetStatePropertyAll(const Size.fromHeight(48)),
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+        foregroundColor: WidgetStatePropertyAll(
+          AppColors.primary,
+        ), // text + icon
+        side: WidgetStatePropertyAll(BorderSide(color: AppColors.primary)),
+      ),
+    ),
+
+    // Text Button
+    textButtonTheme: TextButtonThemeData(
+      style: ButtonStyle(
+        foregroundColor: WidgetStatePropertyAll(AppColors.primary),
+      ),
+    ),
+  );
+}
