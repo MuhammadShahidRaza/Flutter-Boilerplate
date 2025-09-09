9946334e8c8f028501e13d25d1dccd27a52e11b4
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-10 01:14:50 +0500

    auth serviuce set

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,22 +2,15 @@ import 'package:sanam_laundry/constants/variables.dart';
 import 'package:sanam_laundry/utils/storage.dart';
 
 class AuthService {
-  static String? _token;
-
-  static Future<void> loadToken() async {
-    final token = await Storage.getDataFromStorage(Variables.userToken);
-    _token = token;
+  static Future<String?> loadToken() async {
+    return await Storage.getDataFromStorage(Variables.userToken);
   }
 
-  static bool get isLoggedIn => _token != null && _token!.isNotEmpty;
-
   static Future<void> saveToken(String token) async {
-    _token = token;
     await Storage.addDataToStorage(Variables.userToken, token);
   }
 
   static Future<void> removeToken() async {
-    _token = null;
     await Storage.deleteDataFromStorage(Variables.userToken);
   }
 }
