dc2e7a4ce5de3814b85904bab4172638af6d8803
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-25 23:07:31 +0500

    assets updated

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,16 @@
+class AppAssets {
+  // Videos
+  static const splashVideo = 'assets/videos/splash.mov';
+
+  // Images
+
+  //COMMON
+
+  static const logo = 'assets/images/common/logo.png';
+
+  // Auth
+
+  static const onboardingOne = 'assets/images/auth/onboarding_one.png';
+  static const onboardingTwo = 'assets/images/auth/onboarding_two.png';
+  static const onboardingThree = 'assets/images/auth/onboarding_three.png';
+}
