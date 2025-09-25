64d27cafd8831aba1191378c8839a6a3b1778010
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-25 23:08:37 +0500

    environment updated

---
+++ lib/core/config/environment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Environment {
+  static String get fileName {
+    if (kReleaseMode) {
+      return ".env.production";
+    }
+    return ".env.development";
+  }
+
+  static String get baseUrl => dotenv.env['API_BASE_URL'] ?? '';
+  static bool get enableLogs => dotenv.env['ENABLE_LOGS'] == 'true';
+  static String get mapKey => dotenv.env['MAP_API_KEY'] ?? '';
+}
