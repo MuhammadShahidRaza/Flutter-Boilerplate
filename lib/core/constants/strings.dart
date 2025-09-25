da13d1311a8e04b5c53d456a79f968f000d9dc1b
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-25 23:08:56 +0500

    strings updated

---
+++ lib/core/constants/strings.dart
@@ -0,0 +1,370 @@
+class Common {
+  static const email = "COMMON.EMAIL";
+  static const feild = "COMMON.FEILD";
+  static const password = "COMMON.PASSWORD";
+  static const forgotPassword = "COMMON.FORGOT_PASSWORD";
+  static const resetPassword = "COMMON.RESET_PASSWORD";
+  static const signUp = "COMMON.SIGN_UP";
+  static const signIn = "COMMON.SIGN_IN";
+  static const login = "COMMON.LOGIN";
+  static const newPassword = "COMMON.NEW_PASSWORD";
+  static const confirmation = "COMMON.CONFIRMATION";
+  static const enter = "COMMON.ENTER";
+  static const fullName = "COMMON.FULL_NAME";
+  static const enterFullName = "COMMON.ENTER_FULL_NAME";
+  static const noInternetConnection = "COMMON.NO_INTERNET_CONNECTION";
+  static const country = "COMMON.COUNTRY";
+  static const enterCountry = "COMMON.ENTER_COUNTRY";
+  static const enterCurrentPassword = "COMMON.ENTER_CURRENT_PASSWORD";
+  static const enterUserName = "COMMON.ENTER_USER_NAME";
+  static const emailOrUsername = "COMMON.EMAIL_OR_USERNAME";
+  static const somethingWentWrong = "COMMON.SOMETHING_WENT_WRONG";
+  static const summary = "COMMON.SUMMARY";
+  static const greetings = "COMMON.GREETINGS";
+  static const enterYourMessage = "COMMON.ENTER_YOUR_MESSAGE";
+  static const helpAndSupport = "COMMON.HELP_AND_SUPPORT";
+  static const allowLocation = "COMMON.ALLOW_LOCATION";
+  static const letsConnect = "COMMON.LETS_CONNECT";
+  static const deactivateAccount = "COMMON.DEACTIVATE_ACCOUNT";
+  static const timeDuration = "COMMON.TIME_DURATION";
+  static const price = "COMMON.PRICE";
+  static const platformFee = "COMMON.PLATFORM_FEE";
+  static const total = "COMMON.TOTAL";
+  static const about = "COMMON.ABOUT";
+  static const availability = "COMMON.AVAILABILITY";
+  static const paymentOptions = "COMMON.PAYMENT_OPTIONS";
+  static const timings = "COMMON.TIMINGS";
+  static const remarks = "COMMON.REMARKS";
+  static const today = "COMMON.TODAY";
+  static const editProfile = "COMMON.EDIT_PROFILE";
+  static const securelyLogout = "COMMON.SECURELY_LOGOUT";
+  static const aboutUs = "COMMON.ABOUT_US";
+  static const veryBad = "COMMON.VERY_BAD";
+  static const bad = "COMMON.BAD";
+  static const average = "COMMON.AVERAGE";
+  static const good = "COMMON.GOOD";
+  static const veryGood = "COMMON.VERY_GOOD";
+  static const review = "COMMON.REVIEW";
+  static const morning = "COMMON.MORNING";
+  static const evening = "COMMON.EVENING";
+  static const afternoon = "COMMON.AFTERNOON";
+  static const readLess = "COMMON.READ_LESS";
+  static const readMore = "COMMON.READ_MORE";
+  static const recommendations = "COMMON.RECOMMENDATIONS";
+  static const add = "COMMON.ADD";
+  static const packages = "COMMON.PACKAGES";
+  static const package = "COMMON.PACKAGE";
+  static const services = "COMMON.SERVICES";
+  static const confirmPassword = "COMMON.CONFIRM_PASSWORD";
+  static const phoneNumber = "COMMON.PHONE_NUMBER";
+  static const firstName = "COMMON.FIRST_NAME";
+  static const pay = "COMMON.PAY";
+  static const signature = "COMMON.SIGNATURE";
+  static const letsStart = "COMMON.LETS_START";
+  static const lastName = "COMMON.LAST_NAME";
+  static const rememberMe = "COMMON.REMEMBER_ME";
+  static const select = "COMMON.SELECT";
+  static const skip = "COMMON.SKIP";
+  static const choosePaymentMethod = "COMMON.CHOOSE_PAYMENT_METHOD";
+  static const duration = "COMMON.DURATION";
+  static const filters = "COMMON.FILTERS";
+  static const radius = "COMMON.RADIUS";
+  static const individual = "COMMON.INDIVIDUAL";
+  static const company = "COMMON.COMPANY";
+  static const payNow = "COMMON.PAY_NOW";
+  static const enterCardNumber = "COMMON.ENTER_CARD_NUMBER";
+  static const enterYourCvv = "COMMON.ENTER_YOUR_CVV";
+  static const enterExpiryDate = "COMMON.ENTER_EXPIRY_DATE";
+  static const payment = "COMMON.PAYMENT";
+  static const proceedToPayment = "COMMON.PROCEED_TO_PAYMENT";
+  static const viewFullProfile = "COMMON.VIEW_FULL_PROFILE";
+  static const therapyType = "COMMON.THERAPY_TYPE";
+  static const specialization = "COMMON.SPECIALIZATION";
+  static const logout = "COMMON.LOGOUT";
+  static const yes = "COMMON.YES";
+  static const no = "COMMON.NO";
+  static const save = "COMMON.SAVE";
+  static const experience = "COMMON.EXPERIENCE";
+  static const sessions = "COMMON.SESSIONS";
+  static const reviews = "COMMON.REVIEWS";
+  static const fee = "COMMON.FEE";
+  static const seeMore = "COMMON.SEE_MORE";
+  static const viewMap = "COMMON.VIEW_MAP";
+  static const cancel = "COMMON.CANCEL";
+  static const days = "COMMON.DAYS";
+  static const submit = "COMMON.SUBMIT";
+  static const update = "COMMON.UPDATE";
+  static const sendOtp = "COMMON.SEND_OTP";
+  static const delete = "COMMON.DELETE";
+  static const search = "COMMON.SEARCH";
+  static const clear = "COMMON.CLEAR";
+  static const next = "COMMON.NEXT";
+  static const previous = "COMMON.PREVIOUS";
+  static const welcome = "COMMON.WELCOME";
+  static const loading = "COMMON.LOADING";
+  static const noResults = "COMMON.NO_RESULTS";
+  static const chooseLanguage = "COMMON.CHOOSE_LANGUAGE";
+  static const termsAndConditions = "COMMON.TERMS_AND_CONDITIONS";
+  static const privacyPolicy = "COMMON.PRIVACY_POLICY";
+  static const profile = "COMMON.PROFILE";
+  static const noItemFound = "COMMON.NO_ITEM_FOUND";
+  static const settings = "COMMON.SETTINGS";
+  static const help = "COMMON.HELP";
+  static const feedback = "COMMON.FEEDBACK";
+  static const notifications = "COMMON.NOTIFICATIONS";
+  static const language = "COMMON.LANGUAGE";
+  static const changePassword = "COMMON.CHANGE_PASSWORD";
+  static const account = "COMMON.ACCOUNT";
+  static const dashboard = "COMMON.DASHBOARD";
+  static const viewDetails = "COMMON.VIEW_DETAILS";
+  static const edit = "COMMON.EDIT";
+  static const share = "COMMON.SHARE";
+  static const viewMore = "COMMON.VIEW_MORE";
+  static const contactUs = "COMMON.CONTACT_US";
+  static const subscribe = "COMMON.SUBSCRIBE";
+  static const unsubscribe = "COMMON.UNSUBSCRIBE";
+  static const freeTrial = "COMMON.FREE_TRIAL";
+  static const activate = "COMMON.ACTIVATE";
+  static const deactivate = "COMMON.DEACTIVATE";
+  static const confirm = "COMMON.CONFIRM";
+  static const languages = "COMMON.LANGUAGES";
+  static const awards = "COMMON.AWARDS";
+  static const education = "COMMON.EDUCATION";
+  static const retry = "COMMON.RETRY";
+  static const sessionExpired = "COMMON.SESSION_EXPIRED";
+  static const createAnAccount = "COMMON.CREATE_AN_ACCOUNT";
+  static const enterYourEmail = "COMMON.ENTER_YOUR_EMAIL";
+  static const enterYourName = "COMMON.ENTER_YOUR_NAME";
+  static const enterYourPassword = "COMMON.ENTER_YOUR_PASSWORD";
+  static const enterYourPhoneNumber = "COMMON.ENTER_YOUR_PHONE_NUMBER";
+  static const enterFirstName = "COMMON.ENTER_FIRST_NAME";
+  static const enterLastName = "COMMON.ENTER_LAST_NAME";
+  static const enterNewPassword = "COMMON.ENTER_NEW_PASSWORD";
+  static const enterConfirmPassword = "COMMON.ENTER_CONFIRM_PASSWORD";
+  static const verificationCode = "COMMON.VERIFICATION_CODE";
+  static const cvv = "COMMON.CVV";
+  static const cardNumber = "COMMON.CARD_NUMBER";
+  static const expiryDate = "COMMON.EXPIRY_DATE";
+  static const goodMorning = "COMMON.GOOD_MORNING";
+  static const goodEvening = "COMMON.GOOD_EVENING";
+  static const goodAfternoon = "COMMON.GOOD_AFTERNOON";
+  static const currentPassword = "COMMON.CURRENT_PASSWORD";
+  static const name = "COMMON.NAME";
+  static const upcoming = "COMMON.UPCOMING";
+  static const past = "COMMON.PAST";
+  static const custom = "COMMON.CUSTOM";
+  static const completed = "COMMON.COMPLETED";
+  static const location = "COMMON.LOCATION";
+  static const writeMessage = "COMMON.WRITE_MESSAGE";
+  static const bio = "COMMON.BIO";
+  static const message = "COMMON.MESSAGE";
+  static const messages = "COMMON.MESSAGES";
+  static const description = "COMMON.DESCRIPTION";
+  static const username = "COMMON.USERNAME";
+  static const orContinueWith = "COMMON.OR_CONTINUE_WITH";
+  static const facebook = "COMMON.FACEBOOK";
+  static const google = "COMMON.GOOGLE";
+  static const dontHaveAnAccount = "COMMON.DONT_HAVE_AN_ACCOUNT";
+  static const enterYourLocation = "COMMON.ENTER_YOUR_LOCATION";
+  static const alreadyHaveAnAccount = "COMMON.ALREADY_HAVE_AN_ACCOUNT";
+  static const register = "COMMON.REGISTER";
+  static const confirmDelete = "COMMON.CONFIRM_DELETE";
+  static const actionCannotBeUndone = "COMMON.ACTION_CANNOT_BE_UNDONE";
+  static const close = "COMMON.CLOSE";
+  static const yesterday = "COMMON.YESTERDAY";
+  static const tomorrow = "COMMON.TOMORROW";
+  static const justNow = "COMMON.JUST_NOW";
+  static const minutesAgo = "COMMON.MINUTES_AGO";
+  static const hoursAgo = "COMMON.HOURS_AGO";
+  static const newNotification = "COMMON.NEW_NOTIFICATION";
+  static const viewAll = "COMMON.VIEW_ALL";
+  static const markAllAsRead = "COMMON.MARK_ALL_AS_READ";
+  static const tryAgain = "COMMON.TRY_AGAIN";
+  static const whatsOnMind = "COMMON.WHATS_ON_MIND";
+  static const tryingToExitConfirmation = "COMMON.TRYING_TO_EXIT_CONFIRMATION";
+  static const enter4Digit = "COMMON.ENTER_4_DIGIT";
+  static const recoveryCode = "COMMON.RECOVERY_CODE";
+  static const needAHelp = "COMMON.NEED_A_HELP";
+  static const reSendCode = "COMMON.RE_SEND_CODE";
+  static const verify = "COMMON.VERIFY";
+  static const enterVerificationCodeYou = "COMMON.ENTER_VERIFICATION_CODE_YOU";
+  static const resetYourPasswordWith = "COMMON.RESET_YOUR_PASSWORD_WITH";
+  static const didNotYouRecieve = "COMMON.DID_NOT_YOU_RECIEVE";
+}
+
+// --- Ab baki sections ke liye alag classes --- //
+
+class Months {
+  static const january = "MONTHS.JANUARY";
+  static const february = "MONTHS.FEBRUARY";
+  static const march = "MONTHS.MARCH";
+  static const april = "MONTHS.APRIL";
+  static const may = "MONTHS.MAY";
+  static const june = "MONTHS.JUNE";
+  static const july = "MONTHS.JULY";
+  static const august = "MONTHS.AUGUST";
+  static const september = "MONTHS.SEPTEMBER";
+  static const october = "MONTHS.OCTOBER";
+  static const november = "MONTHS.NOVEMBER";
+  static const december = "MONTHS.DECEMBER";
+}
+
+class Weekdays {
+  static const sun = "WEEKDAYS.SUN";
+  static const mon = "WEEKDAYS.MON";
+  static const tue = "WEEKDAYS.TUE";
+  static const wed = "WEEKDAYS.WED";
+  static const thu = "WEEKDAYS.THU";
+  static const fri = "WEEKDAYS.FRI";
+  static const sat = "WEEKDAYS.SAT";
+  static const sunday = "WEEKDAYS.SUNDAY";
+  static const monday = "WEEKDAYS.MONDAY";
+  static const tuesday = "WEEKDAYS.TUESDAY";
+  static const wednesday = "WEEKDAYS.WEDNESDAY";
+  static const thursday = "WEEKDAYS.THURSDAY";
+  static const friday = "WEEKDAYS.FRIDAY";
+  static const saturday = "WEEKDAYS.SATURDAY";
+}
+
+class Onboarding {
+  static const heading1 = "ONBOARDING.HEADING_1";
+  static const heading2 = "ONBOARDING.HEADING_2";
+  static const heading3 = "ONBOARDING.HEADING_3";
+  static const description1 = "ONBOARDING.DESCRIPTION_1";
+  static const description2 = "ONBOARDING.DESCRIPTION_2";
+  static const description3 = "ONBOARDING.DESCRIPTION_3";
+}
+
+class Auth {
+  static const welcomeBackLogin = "AUTH.WELCOME_BACK_LOGIN";
+  static const helloAgainLogin = "AUTH.HELLO_AGAIN_LOGIN";
+  static const dontWorryForgot = "AUTH.DONT_WORRY_FORGOT";
+  static const resetYourPassword = "AUTH.RESET_YOUR_PASSWORD";
+  static const connectWithSignup = "AUTH.CONNECT_WITH_SIGNUP";
+}
+
+class Chat {
+  static const heading1 = "CHAT.HEADING_1";
+  static const heading2 = "CHAT.HEADING_2";
+  static const heading3 = "CHAT.HEADING_3";
+  static const description1 = "CHAT.DESCRIPTION_1";
+  static const description2 = "CHAT.DESCRIPTION_2";
+  static const description3 = "CHAT.DESCRIPTION_3";
+}
+
+class SettingsText {
+  static const locationAccess = "SETTINGS_TEXT.LOCATION_ACCESS";
+  static const allowLocationDescription =
+      "SETTINGS_TEXT.ALLOW_LOCATION_DESCRIPTION";
+  static const weAreHereToHelp = "SETTINGS_TEXT.WE_ARE_HERE_TO_HELP";
+}
+
+class ErrorMessages {
+  static const invalidCredentials = "ERROR_MESSAGES.INVALID_CREDENTIALS";
+  static const accessDenied = "ERROR_MESSAGES.ACCESS_DENIED";
+  static const internalServerError = "ERROR_MESSAGES.INTERNAL_SERVER_ERROR";
+  static const resourceNotFound = "ERROR_MESSAGES.RESOURCE_NOT_FOUND";
+  static const invalidRequest = "ERROR_MESSAGES.INVALID_REQUEST";
+  static const unauthorizedAccess = "ERROR_MESSAGES.UNAUTHORIZED_ACCESS";
+  static const invalidParameters = "ERROR_MESSAGES.INVALID_PARAMETERS";
+  static const operationFailed = "ERROR_MESSAGES.OPERATION_FAILED";
+  static const permissionDenied = "ERROR_MESSAGES.PERMISSION_DENIED";
+  static const invalidToken = "ERROR_MESSAGES.INVALID_TOKEN";
+  static const missingParameters = "ERROR_MESSAGES.MISSING_PARAMETERS";
+  static const notImplemented = "ERROR_MESSAGES.NOT_IMPLEMENTED";
+  static const unexpectedError = "ERROR_MESSAGES.UNEXPECTED_ERROR";
+  static const badRequest = "ERROR_MESSAGES.BAD_REQUEST";
+  static const forbidden = "ERROR_MESSAGES.FORBIDDEN";
+  static const serviceUnavailable = "ERROR_MESSAGES.SERVICE_UNAVAILABLE";
+  static const invalidOperation = "ERROR_MESSAGES.INVALID_OPERATION";
+  static const invalidInput = "ERROR_MESSAGES.INVALID_INPUT";
+  static const timeoutError = "ERROR_MESSAGES.TIMEOUT_ERROR";
+  static const notFound = "ERROR_MESSAGES.NOT_FOUND";
+  static const authenticationFailed = "ERROR_MESSAGES.AUTHENTICATION_FAILED";
+  static const conflictError = "ERROR_MESSAGES.CONFLICT_ERROR";
+  static const databaseError = "ERROR_MESSAGES.DATABASE_ERROR";
+  static const connectionError = "ERROR_MESSAGES.CONNECTION_ERROR";
+  static const invalidAction = "ERROR_MESSAGES.INVALID_ACTION";
+  static const invalidUser = "ERROR_MESSAGES.INVALID_USER";
+  static const invalidData = "ERROR_MESSAGES.INVALID_DATA";
+  static const missingResource = "ERROR_MESSAGES.MISSING_RESOURCE";
+  static const unprocessableEntity = "ERROR_MESSAGES.UNPROCESSABLE_ENTITY";
+  static const unauthorizedRequest = "ERROR_MESSAGES.UNAUTHORIZED_REQUEST";
+  static const invalidResponse = "ERROR_MESSAGES.INVALID_RESPONSE";
+  static const unexpectedResponse = "ERROR_MESSAGES.UNEXPECTED_RESPONSE";
+  static const missingPermissions = "ERROR_MESSAGES.MISSING_PERMISSIONS";
+  static const networkError = "ERROR_MESSAGES.NETWORK_ERROR";
+  static const invalidFormat = "ERROR_MESSAGES.INVALID_FORMAT";
+  static const invalidUrl = "ERROR_MESSAGES.INVALID_URL";
+  static const invalidFile = "ERROR_MESSAGES.INVALID_FILE";
+  static const invalidConfiguration = "ERROR_MESSAGES.INVALID_CONFIGURATION";
+  static const invalidEmail = "ERROR_MESSAGES.INVALID_EMAIL";
+  static const invalidPassword = "ERROR_MESSAGES.INVALID_PASSWORD";
+  static const invalidUsername = "ERROR_MESSAGES.INVALID_USERNAME";
+  static const invalidRole = "ERROR_MESSAGES.INVALID_ROLE";
+  static const invalidResource = "ERROR_MESSAGES.INVALID_RESOURCE";
+  static const invalidField = "ERROR_MESSAGES.INVALID_FIELD";
+  static const invalidContent = "ERROR_MESSAGES.INVALID_CONTENT";
+  static const invalidId = "ERROR_MESSAGES.INVALID_ID";
+  static const invalidSession = "ERROR_MESSAGES.INVALID_SESSION";
+  static const invalidDomain = "ERROR_MESSAGES.INVALID_DOMAIN";
+}
+
+class ValidationMessages {
+  static const invalidEmailFormat = "VALIDATION_MESSAGES.INVALID_EMAIL_FORMAT";
+  static const invalidPhoneNumber = "VALIDATION_MESSAGES.INVALID_PHONE_NUMBER";
+  static const wrongPhoneNumber = "VALIDATION_MESSAGES.WRONG_PHONE_NUMBER";
+  static const invalidUsername = "VALIDATION_MESSAGES.INVALID_USERNAME";
+  static const invalidVerificationCode =
+      "VALIDATION_MESSAGES.INVALID_VERIFICATION_CODE";
+  static const invalidNameWithHyphen =
+      "VALIDATION_MESSAGES.INVALID_NAME_WITH_HYPHEN";
+  static const invalidMobileNumberMin =
+      "VALIDATION_MESSAGES.INVALID_MOBILE_NUMBER_MIN";
+  static const invalidMobileNumberMax =
+      "VALIDATION_MESSAGES.INVALID_MOBILE_NUMBER_MAX";
+  static const passwordMustContain =
+      "VALIDATION_MESSAGES.PASSWORD_MUST_CONTAIN";
+  static const invalidNewPassword = "VALIDATION_MESSAGES.INVALID_NEW_PASSWORD";
+  static const passwordsNotMatch = "VALIDATION_MESSAGES.PASSWORDS_NOT_MATCH";
+  static const invalidCreditCard = "VALIDATION_MESSAGES.INVALID_CREDIT_CARD";
+  static const invalidCvv = "VALIDATION_MESSAGES.INVALID_CVV";
+  static const invalidExpiryDate = "VALIDATION_MESSAGES.INVALID_EXPIRY_DATE";
+  static const invalidNameMin = "VALIDATION_MESSAGES.INVALID_NAME_MIN";
+  static const invalidNameMax = "VALIDATION_MESSAGES.INVALID_NAME_MAX";
+  static const invalidBioMin = "VALIDATION_MESSAGES.INVALID_BIO_MIN";
+  static const invalidBioMax = "VALIDATION_MESSAGES.INVALID_BIO_MAX";
+  static const invalidDescriptionMin =
+      "VALIDATION_MESSAGES.INVALID_DESCRIPTION_MIN";
+  static const invalidDescriptionMax =
+      "VALIDATION_MESSAGES.INVALID_DESCRIPTION_MAX";
+  static const invalidUrlFormat = "VALIDATION_MESSAGES.INVALID_URL_FORMAT";
+  static const minLength = "VALIDATION_MESSAGES.MIN_LENGTH";
+  static const maxLength = "VALIDATION_MESSAGES.MAX_LENGTH";
+  static const isRequired = "VALIDATION_MESSAGES.IS_REQUIRED";
+  static const passwordsMustMatch = "VALIDATION_MESSAGES.PASSWORDS_MUST_MATCH";
+}
+
+class TemporaryText {
+  static const lorumIpsum = "TEMPORARY_TEXT.LORUM_IPSUM";
+  static const lorumIpsumLong = "TEMPORARY_TEXT.LORUM_IPSUM_LONG";
+  static const lorumIpsumTooLong = "TEMPORARY_TEXT.LORUM_IPSUM_TOO_LONG";
+  static const time = "TEMPORARY_TEXT.TIME";
+  static const rating = "TEMPORARY_TEXT.RATING";
+  static const three = "TEMPORARY_TEXT.THREE";
+  static const price = "TEMPORARY_TEXT.PRICE";
+  static const fiveHundred = "TEMPORARY_TEXT.FIVE_HUNDRED";
+  static const days3 = "TEMPORARY_TEXT.DAYS_3";
+  static const johnDoe = "TEMPORARY_TEXT.JOHN_DOE";
+  static const min30 = "TEMPORARY_TEXT.MIN_30";
+  static const min60 = "TEMPORARY_TEXT.MIN_60";
+  static const dollors60 = "TEMPORARY_TEXT.DOLLORS_60";
+  static const dollors120 = "TEMPORARY_TEXT.DOLLORS_120";
+  static const fiveYears = "TEMPORARY_TEXT.FIVE_YEARS";
+  static const add = "TEMPORARY_TEXT.ADD";
+  static const fiftyDeposited = "TEMPORARY_TEXT.FIFTY_DEPOSITED";
+  static const availability = "TEMPORARY_TEXT.AVAILABILITY";
+  static const timings = "TEMPORARY_TEXT.TIMINGS";
+  static const email = "TEMPORARY_TEXT.EMAIL";
+  static const version = "TEMPORARY_TEXT.VERSION";
+}
