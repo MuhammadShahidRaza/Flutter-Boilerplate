fe38249c217fc300789002f1f3c6abfb293b5609
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-25 23:06:50 +0500

    app stack updated

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:sanam_laundry/l10n/setup.dart';
-import 'package:sanam_laundry/routes/router.dart';
-import 'package:sanam_laundry/theme/app_theme.dart';
+import 'package:sanam_laundry/localization/setup.dart';
+import 'package:sanam_laundry/core/routes/router.dart';
+import 'package:sanam_laundry/presentation/theme/app_theme.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,11 +12,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      // locale: const Locale('en'),
+      themeMode: ThemeMode.light,
+      locale: const Locale('en'),
       supportedLocales: AppLocalizationSetup.supportedLocales,
       localizationsDelegates: AppLocalizationSetup.localizationsDelegates,
       localeResolutionCallback: AppLocalizationSetup.resolveLocale,
+      builder: (context, child) {
+        return SafeArea(bottom: false, child: child!); // 👈 Global SafeArea
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:sanam_laundry/app.dart';
+import 'package:sanam_laundry/core/config/environment.dart';
 import 'package:sanam_laundry/providers/index.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: Environment.fileName);
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.dark,
+    ),
+  );
 
   runApp(
     MultiProvider(providers: AppProviders.allProviders, child: const MyApp()),
