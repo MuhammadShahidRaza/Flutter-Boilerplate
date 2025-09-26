55174e620380958293923989dc22385751b10e31
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-26 21:59:04 +0500

    fonts disable

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:sanam_laundry/app.dart';
 import 'package:sanam_laundry/core/config/environment.dart';
@@ -9,14 +9,14 @@ import 'package:sanam_laundry/providers/index.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: Environment.fileName);
-
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.white,
-      statusBarIconBrightness: Brightness.dark,
-      statusBarBrightness: Brightness.dark,
-    ),
-  );
+  GoogleFonts.config.allowRuntimeFetching = false;
+  // SystemChrome.setSystemUIOverlayStyle(
+  //   const SystemUiOverlayStyle(
+  //     statusBarColor: Colors.white,
+  //     statusBarIconBrightness: Brightness.dark,
+  //     statusBarBrightness: Brightness.dark,
+  //   ),
+  // );
 
   runApp(
     MultiProvider(providers: AppProviders.allProviders, child: const MyApp()),
