86d5315bf7444766792694984b150899b2b61e5e
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-09-26 22:01:10 +0500

    text theme set

--- lib/presentation/theme/text.dart
+++ lib/presentation/theme/text.dart
@@ -1,81 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sanam_laundry/presentation/theme/font.dart';
 
 class CustomTextTheme {
   static TextTheme getTextTheme(Color textColor, Color secondaryColor) {
+    TextStyle base({double? size, FontWeight? weight, Color? color}) {
+      return GoogleFonts.getFont(
+        AppFonts.defaultFont,
+        fontSize: size,
+        fontWeight: weight,
+        color: color,
+      );
+    }
+
     return TextTheme(
-      displayLarge: TextStyle(
-        fontSize: 57,
-        fontWeight: FontWeight.bold,
-        color: textColor,
-      ),
-      displayMedium: TextStyle(
-        fontSize: 45,
-        fontWeight: FontWeight.bold,
-        color: textColor,
-      ),
-      displaySmall: TextStyle(
-        fontSize: 36,
-        fontWeight: FontWeight.w600,
-        color: textColor,
-      ),
-      headlineLarge: TextStyle(
-        fontSize: 32,
-        fontWeight: FontWeight.bold,
-        color: textColor,
-      ),
-      headlineMedium: TextStyle(
-        fontSize: 28,
-        fontWeight: FontWeight.w600,
-        color: textColor,
-      ),
-      headlineSmall: TextStyle(
-        fontSize: 24,
-        fontWeight: FontWeight.w600,
-        color: textColor,
-      ),
-      titleLarge: TextStyle(
-        fontSize: 22,
-        fontWeight: FontWeight.w600,
-        color: textColor,
-      ),
-      titleMedium: TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w500,
-        color: textColor,
-      ),
-      titleSmall: TextStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w500,
-        color: textColor,
-      ),
-      bodyLarge: TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.normal,
-        color: textColor,
-      ),
-      bodyMedium: TextStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.normal,
-        color: textColor,
-      ),
-      bodySmall: TextStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.normal,
+      displayLarge: base(size: 57, weight: FontWeight.bold, color: textColor),
+      displayMedium: base(size: 45, weight: FontWeight.bold, color: textColor),
+      displaySmall: base(size: 36, weight: FontWeight.w600, color: textColor),
+      headlineLarge: base(size: 32, weight: FontWeight.bold, color: textColor),
+      headlineMedium: base(size: 28, weight: FontWeight.bold, color: textColor),
+      headlineSmall: base(size: 24, weight: FontWeight.w600, color: textColor),
+      titleLarge: base(size: 22, weight: FontWeight.w600, color: textColor),
+      titleMedium: base(size: 16, weight: FontWeight.w500, color: textColor),
+      titleSmall: base(size: 14, weight: FontWeight.w500, color: textColor),
+      bodyLarge: base(size: 16, weight: FontWeight.normal, color: textColor),
+      bodyMedium: base(size: 14, weight: FontWeight.normal, color: textColor),
+      bodySmall: base(
+        size: 12,
+        weight: FontWeight.normal,
         color: secondaryColor,
       ),
-      labelLarge: TextStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w500,
-        color: textColor,
-      ),
-      labelMedium: TextStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.w500,
+      labelLarge: base(size: 14, weight: FontWeight.w500, color: textColor),
+      labelMedium: base(
+        size: 12,
+        weight: FontWeight.w500,
         color: secondaryColor,
       ),
-      labelSmall: TextStyle(
-        fontSize: 10,
-        fontWeight: FontWeight.w500,
+      labelSmall: base(
+        size: 10,
+        weight: FontWeight.w500,
         color: secondaryColor,
       ),
     );
