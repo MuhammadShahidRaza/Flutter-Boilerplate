d22b55c7356840fb6280ca7b628a3eb09929fbc8
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-10-14 00:25:39 +0500

    assets

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -14,4 +14,5 @@ class AppAssets {
   static const onboardingTwo = 'assets/images/auth/onboarding_two.png';
   static const onboardingThree = 'assets/images/auth/onboarding_three.png';
   static const watermark = 'assets/images/auth/watermark.png';
+  static const getStarted = 'assets/images/auth/getStarted.png';
 }
