28656c16bcd6739ea3df00cf1a21b87ade7f905e
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-10-20 23:56:45 +0500

    assets added

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -5,15 +5,17 @@ class AppAssets {
   // Images
 
   //COMMON
-  static const imagePath = "assets/images/";
+  static const _imagePath = "assets/images/";
 
-  static const logo = "${imagePath}common/logo.png";
-  static const user = "${imagePath}common/user.png";
+  static const logo = "${_imagePath}common/logo.png";
+  static const user = "${_imagePath}common/user.png";
+  static const rightTick = "${_imagePath}common/right.png";
 
   // AUTH
-  static const onboardingOne = "${imagePath}auth/onboarding_one.png";
-  static const onboardingTwo = "${imagePath}auth/onboarding_two.png";
-  static const onboardingThree = "${imagePath}auth/onboarding_three.png";
-  static const watermark = "${imagePath}auth/watermark.png";
-  static const getStarted = "${imagePath}auth/getStarted.png";
+  static const onboardingOne = "${_imagePath}auth/onboarding_one.png";
+  static const onboardingTwo = "${_imagePath}auth/onboarding_two.png";
+  static const onboardingThree = "${_imagePath}auth/onboarding_three.png";
+  static const watermark = "${_imagePath}auth/watermark.png";
+  static const getStarted = "${_imagePath}auth/getStarted.png";
+  static const allSet = "${_imagePath}auth/allSet.png";
 }
