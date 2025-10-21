7d1d010f600772aaf6512a10eebe475895a3b460
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-10-22 01:45:18 +0500

    light fixed

--- lib/app.dart
+++ lib/app.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
       locale: const Locale('en'),
       supportedLocales: AppLocalizationSetup.supportedLocales,
       localizationsDelegates: AppLocalizationSetup.localizationsDelegates,
