de872378e579ee9d1b3782d0df71d3aff7820ba5
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-10-23 01:44:01 +0500

    endpoints added

--- lib/data/services/endpoints.dart
+++ lib/data/services/endpoints.dart
@@ -1,19 +1,16 @@
 class Endpoints {
-  static const String baseUrl = "https://api.yourapp.com";
-
   // Auth
-  static const String login = "$baseUrl/auth/login";
-  static const String register = "$baseUrl/auth/register";
-  static const String logout = "$baseUrl/auth/logout";
+  static const String login = 'login';
+  static const String register = 'register';
+  static const String refreshToken = 'refresh';
+  static const String profile = 'profile';
 
   // User
-  static const String getUserProfile = "$baseUrl/user/profile";
-  static const String updateUserProfile = "$baseUrl/user/update";
+  static const String getUserProfile = "/user/profile";
+  static const String updateUserProfile = "/user/update";
 
   // Orders
-  static const String getOrders = "$baseUrl/orders";
-  static const String createOrder = "$baseUrl/orders/create";
-  static const String updateOrder = "$baseUrl/orders/update";
-
-  // Add more endpoints here
+  static const String getOrders = "/orders";
+  static const String createOrder = "/orders/create";
+  static const String updateOrder = "/orders/update";
 }
