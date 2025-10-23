846e2c2fbb76ed1f5aafb622ba2ee6433924e773
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-10-24 02:01:58 +0500

    user data saved

--- lib/data/services/auth.dart
+++ lib/data/services/auth.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:sanam_laundry/core/constants/variables.dart';
 import 'package:sanam_laundry/core/utils/storage.dart';
+import 'package:sanam_laundry/data/index.dart';
 
 class AuthService {
   static Future<String?> loadToken() async {
@@ -20,5 +23,21 @@ class AuthService {
 
   static Future<void> removeToken() async {
     await Storage.deleteDataFromStorage(Variables.userToken);
+  }
+
+  static Future<void> removeUserData() async {
+    await Storage.deleteDataFromStorage(Variables.userData);
+  }
+
+  static Future<void> saveUser(UserModel user) async {
+    final userJson = jsonEncode(user.toJson());
+    await Storage.addDataToStorage(Variables.userData, userJson);
+  }
+
+  static Future<UserModel?> loadUser() async {
+    final userData = await Storage.getDataFromStorage(Variables.userData);
+    if (userData == null || userData.isEmpty) return null;
+    final jsonData = jsonDecode(userData);
+    return UserModel.fromJson(jsonData);
   }
 }
