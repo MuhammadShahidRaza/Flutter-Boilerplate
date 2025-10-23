03bad9efc1a4c840486d203d7d92e33c9ad54d22
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-10-24 02:00:42 +0500

    routes added

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -9,9 +9,22 @@ class AppRoutes {
 
   // Bottom Navigation route names
   static const String home = '/';
+  static const String myAccount = '/my_account';
+  static const String cart = '/cart';
+  static const String chat = '/chat';
+  static const String notifications = '/notifications';
+  static const String orderHistory = '/order_history';
+  static const String editProfile = '/edit_profile';
+  static const String myOrders = '/my_orders';
+  static const String myAddress = '/my_address';
+  static const String payment = '/payment';
+  static const String language = '/language';
+  static const String contactUs = '/contact_us';
+  static const String termsAndConditions = '/terms_and_conditions';
+  static const String privacyPolicy = '/privacy_policy';
+
   static const String orders = '/orders';
   static const String services = '/services';
-  static const String myAccount = '/my_account';
 
   // App Navigation route names
   static const String profile = '/profile';
