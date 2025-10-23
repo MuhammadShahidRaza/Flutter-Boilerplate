acf5f8e0c8092faa27e31ce4040cdd604ab58b0d
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-10-24 02:06:33 +0500

    verfication done

--- lib/presentation/screens/auth/verification.dart
+++ lib/presentation/screens/auth/verification.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sanam_laundry/core/index.dart';
+import 'package:sanam_laundry/data/index.dart';
 import 'package:sanam_laundry/presentation/index.dart';
+import 'package:sanam_laundry/providers/auth.dart';
 
 class Verification extends StatefulWidget {
   const Verification({super.key});
@@ -14,7 +16,10 @@ class Verification extends StatefulWidget {
 class _VerificationState extends State<Verification> {
   final _formKey = GlobalKey<FormState>();
   String _otpCode = "";
+  final AuthRepository _authRepository = AuthRepository();
+
   bool _isOtpComplete = false;
+  bool loading = false;
 
   int _secondsRemaining = 60;
   Timer? _timer;
@@ -43,47 +48,61 @@ class _VerificationState extends State<Verification> {
     super.dispose();
   }
 
-  void _submit() {
+  Future<void> _submit() async {
     if (!_isOtpComplete) return;
-
-    // Handle OTP verification here
-    print("Verifying OTP: $_otpCode");
-
-    // AppDialog.show(
-    //   context,
-    //   title: Common.successfullyLoggedIn,
-    //   imagePath: AppAssets.rightTick,
-    //   spacing: Dimens.spacingMSmall,
-    //   content: AppText(
-    //     maxLines: 3,
-    //     textAlign: TextAlign.center,
-    //     Auth.gladToHadYouBack,
-    //   ),
-    //   crossAxisAlignment: CrossAxisAlignment.center,
-    //   insetPadding: EdgeInsets.all(Dimens.spacingXXL),
-    // );
-
-    // AppDialog.show(
-    //   context,
-    //   title: Auth.okayAllSet,
-    //   imagePath: AppAssets.allSet,
-    //   borderColor: AppColors.primary,
-    //   borderWidth: 4,
-    //   borderRadius: Dimens.radiusL,
-    //   imageSize: 150,
-    //   content: AppText(
-    //     maxLines: 3,
-    //     textAlign: TextAlign.center,
-    //     Auth.soonYouWillRecieve,
-    //   ),
-    //   primaryButtonText: Auth.letsExploreApp,
-    //   onPrimaryPressed: () => context.replacePage(AppRoutes.home),
-    //   backgroundColor: AppColors.lightWhite,
-    //   crossAxisAlignment: CrossAxisAlignment.center,
-    //   insetPadding: EdgeInsets.all(Dimens.spacingXXL),
-    // );
-
-    // context.replacePage(AppRoutes.home);
+    setState(() => loading = true);
+    final user = await _authRepository.verifyOtp(
+      phone: context.getParam<String>('phone') ?? '',
+      otp: _otpCode,
+      // device_type:Testing Tool
+      // device_token:abcdefghijklmnopqrstuvwxyz
+      // udid:123456789
+      // device_brand:Postman
+      // device_os:Linux
+      // app_version:1.0.0
+    );
+    if (!mounted) return;
+    setState(() => loading = false);
+    final isFromLogin = context.getParam<bool>('isFromLogin') ?? false;
+    if (user != null) {
+      context.read<AuthProvider>().login(user);
+      if (!isFromLogin) {
+        AppDialog.show(
+          context,
+          title: Auth.okayAllSet,
+          imagePath: AppAssets.allSet,
+          borderColor: AppColors.primary,
+          borderWidth: 4,
+          borderRadius: Dimens.radiusL,
+          imageSize: 150,
+          content: AppText(
+            maxLines: 3,
+            textAlign: TextAlign.center,
+            Auth.soonYouWillRecieve,
+          ),
+          primaryButtonText: Auth.letsExploreApp,
+          onPrimaryPressed: () => context.replacePage(AppRoutes.home),
+          backgroundColor: AppColors.lightWhite,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          insetPadding: EdgeInsets.all(Dimens.spacingXXL),
+        );
+      } else {
+        AppDialog.show(
+          context,
+          title: Common.successfullyLoggedIn,
+          imagePath: AppAssets.rightTick,
+          spacing: Dimens.spacingMSmall,
+          content: AppText(
+            maxLines: 3,
+            textAlign: TextAlign.center,
+            Auth.gladToHadYouBack,
+          ),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          insetPadding: EdgeInsets.all(Dimens.spacingXXL),
+        );
+        context.replacePage(AppRoutes.home);
+      }
+    }
   }
 
   void _onOtpCompleted(String code) {
@@ -93,17 +112,18 @@ class _VerificationState extends State<Verification> {
     });
   }
 
-  void _onResendPressed() {
+  void _onResendPressed() async {
     if (_secondsRemaining == 0) {
+      await _authRepository.login(
+        phone: context.getParam<String>('phone') ?? '',
+      );
       _startTimer();
-      // call resend API here if needed
-      print("Resending OTP...");
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final phone = context.getParam<String>('phone') ?? '';
+    // final phone = context.getParam<String>('phone') ?? '';
 
     return AuthWrapper(
       formKey: _formKey,
@@ -111,7 +131,7 @@ class _VerificationState extends State<Verification> {
       title: Common.enterOtp,
       subtitle: Common.enterVerificationCodeYou,
       buttonText: Common.continue_,
-
+      isLoading: loading,
       bottomButtonText: _secondsRemaining == 0 ? Common.reSendCode : "",
       showBackButton: true,
       bottomButtonPress: _onResendPressed,
