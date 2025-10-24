121d6483fc2cd903b96606bcd3387360bed821f3
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-10-25 01:17:40 +0500

    local added

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sanam_laundry/core/index.dart';
 import 'package:sanam_laundry/localization/index.dart';
 import 'package:sanam_laundry/presentation/index.dart';
+import 'package:sanam_laundry/providers/auth.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    final locale = context.watch<AuthProvider>().locale;
     return MaterialApp.router(
       routerConfig: GoRouterSetup.router,
       debugShowCheckedModeBanner: false,
@@ -14,8 +17,8 @@ class MyApp extends StatelessWidget {
       scaffoldMessengerKey: AppToast.scaffoldMessengerKey,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.light,
-      locale: const Locale('en'),
-      supportedLocales: AppLocalizationSetup.supportedLocales,
+      locale: locale,
+      supportedLocales: AppLocalizationSetup().supportedLocales,
       localizationsDelegates: AppLocalizationSetup.localizationsDelegates,
       localeResolutionCallback: AppLocalizationSetup.resolveLocale,
       // builder: (context, child) {
