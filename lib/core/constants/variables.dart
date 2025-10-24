8751069bd9fca782f9549f9deaabe800370198e4
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-10-25 01:18:12 +0500

    variable added

--- lib/core/constants/variables.dart
+++ lib/core/constants/variables.dart
@@ -2,4 +2,5 @@ class Variables {
   static const String userToken = "userToken";
   static const String hasVisitedApp = "hasVisitedApp";
   static const String userData = "userData";
+  static const String languageCode = "languageCode";
 }
