ddd571758f635fe4e50ae4f6a4b2a28a6cd58c40
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-10-31 01:24:13 +0500

    chaeck initail launch

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,12 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:sanam_laundry/app.dart';
-import 'package:sanam_laundry/core/config/environment.dart';
+import 'package:sanam_laundry/core/index.dart';
 import 'package:sanam_laundry/providers/index.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Storage.initialize();
   await dotenv.load(fileName: Environment.fileName);
   GoogleFonts.config.allowRuntimeFetching = true;
   // SystemChrome.setSystemUIOverlayStyle(
