76cf343bb8788fb956e4bf9dd2d72a78a6d82e87
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-11-06 01:28:36 +0500

    import changed

--- lib/localization/setup.dart
+++ lib/localization/setup.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:sanam_laundry/providers/auth.dart';
+import 'package:sanam_laundry/providers/app.dart';
 import 'app_localizations.dart';
 
 class AppLocalizationSetup {
