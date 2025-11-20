211eb2f53e39f065e49ef37b1583b05e737e40f0
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-11-21 00:49:59 +0500

    Refactor ServiceItemModel to improve field organization and ensure required parameters are correctly defined

--- lib/data/models/service.dart
+++ lib/data/models/service.dart
@@ -1,13 +1,33 @@
+import 'package:sanam_laundry/core/index.dart';
+
 class ServiceItemModel {
+  final String id;
   final String title;
   final String subtitle;
-  final String? image;
-  final String id;
+  final String amount;
+  final String image;
+  final String description;
+  final String currency;
 
   ServiceItemModel({
+    required this.id,
     required this.title,
+    required this.amount,
     required this.subtitle,
     required this.image,
-    required this.id,
+    required this.currency,
+    required this.description,
   });
+
+  factory ServiceItemModel.fromJson(Map<String, dynamic> json) {
+    return ServiceItemModel(
+      id: json['id'].toString(),
+      title: json['title'] ?? "",
+      amount: json['amount'] ?? "",
+      subtitle: json['description'] ?? "",
+      description: json['description'] ?? "",
+      image: json['image'] ?? AppAssets.user,
+      currency: json['currency'] ?? "SAR",
+    );
+  }
 }
