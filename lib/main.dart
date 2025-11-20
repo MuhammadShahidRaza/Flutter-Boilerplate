6f65f60c580c84c5b8168dd4bd416e3cd52e3464
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-11-21 00:48:59 +0500

    Replace print with debugPrint for background message logging

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // make sure you call `initializeApp` before using other Firebase services.
   await Firebase.initializeApp();
 
-  print("Handling a background message: ${message.messageId}");
+  debugPrint("Handling a background message: ${message.messageId}");
 }
 
 void main() async {
