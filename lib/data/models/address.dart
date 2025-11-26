14cd5f523a560a1339e2aa4cb0cd75e91f269738
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-11-27 03:33:49 +0500

    Refactor AddressModel to improve JSON parsing and structure by adding missing fields and ensuring required properties are correctly defined.

--- lib/data/models/address.dart
+++ lib/data/models/address.dart
@@ -1,3 +1,5 @@
+import 'package:sanam_laundry/core/utils/helper.dart';
+
 class AddressModel {
   AddressModel({
     required this.id,
@@ -7,14 +9,18 @@ class AddressModel {
     required this.city,
     required this.state,
     required this.latitude,
+    required this.buildingName,
+    required this.floor,
     required this.longitude,
+    required this.buildingImage,
+    required this.apartmentImage,
     required this.isActive,
     required this.isDefault,
     required this.createdAt,
     required this.updatedAt,
   });
 
-  final int? id;
+  final int id;
   final int? userId;
   final String? label;
   final String? address;
@@ -22,25 +28,33 @@ class AddressModel {
   final String? state;
   final String? latitude;
   final String? longitude;
+  final String? floor;
+  final String? buildingName;
   final int? isActive;
   final int? isDefault;
+  final String? buildingImage;
+  final String? apartmentImage;
   final DateTime? createdAt;
   final DateTime? updatedAt;
 
-  factory AddressModel.fromJson(Map<String, dynamic> json) {
+  factory AddressModel.fromJson(Map<dynamic, dynamic> json) {
     return AddressModel(
       id: json["id"],
       userId: json["user_id"],
-      label: json["label"],
-      address: json["address"],
-      city: json["city"],
-      state: json["state"],
-      latitude: json["latitude"],
-      longitude: json["longitude"],
+      label: json["label"] ?? "",
+      address: json["address"] ?? "",
+      buildingName: json["building_name"] ?? "",
+      buildingImage: json["building_image_url"] ?? "",
+      apartmentImage: json["apartment_image_url"] ?? "",
+      floor: json["floor"] ?? "",
+      city: json["city"] ?? "",
+      state: json["state"] ?? "",
+      latitude: json["latitude"] ?? "",
+      longitude: json["longitude"] ?? "",
       isActive: json["is_active"],
       isDefault: json["is_default"],
-      createdAt: DateTime.tryParse(json["created_at"] ?? ""),
-      updatedAt: DateTime.tryParse(json["updated_at"] ?? ""),
+      createdAt: Utils.parseDate(json["created_at"]),
+      updatedAt: Utils.parseDate(json["updated_at"]),
     );
   }
 }
