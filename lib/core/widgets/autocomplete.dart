e23deeb8622287a2c42f35a838c6dbbb9c444fe7
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-11-29 00:55:45 +0500

    Update Google Places API key in AppAutocomplete widget

--- lib/core/widgets/autocomplete.dart
+++ lib/core/widgets/autocomplete.dart
@@ -52,7 +52,7 @@ class _AppAutocompleteState extends State<AppAutocomplete> {
         GooglePlaceAutoCompleteTextField(
           textEditingController: widget.textEditingController,
           // googleAPIKey: Environment.mapKey,
-          googleAPIKey: '""',
+          googleAPIKey: 'AIzaSyANHwsQzGfKQmtizEY7pHApMX4Gvig_OEI',
           inputDecoration: InputDecoration(
             errorMaxLines: 3,
             counterText: widget.counterText,
