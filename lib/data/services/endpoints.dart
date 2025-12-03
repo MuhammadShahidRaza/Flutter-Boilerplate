4660f7f2bfc698895810da2e01aabf9b178c8147
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-12-03 23:49:18 +0500

    Refactor API endpoints in Endpoints class to include 'user/' prefix for consistency and clarity

--- lib/data/services/endpoints.dart
+++ lib/data/services/endpoints.dart
@@ -1,36 +1,35 @@
 class Endpoints {
   // Auth
-  static const String login = 'login';
-  static const String register = 'register';
-  static const String refreshToken = 'refresh';
-  static const String profile = 'user/user';
-  static const String verifyOtp = 'verify-otp';
+  static const String login = 'user/login';
+  static const String register = 'user/register';
+  static const String profile = 'user/user/user';
+  static const String verifyOtp = 'user/verify-otp';
 
   // Home
-  static const String banners = 'banner';
-  static const String categories = 'category';
-  static const String services = 'service';
-  static const String addAddress = 'address/create';
-  static const String updateAddress = 'address';
-  static const String addresses = 'address';
+  static const String banners = 'user/banner';
+  static const String categories = 'user/category';
+  static const String services = 'user/service';
+  static const String addAddress = 'user/address/create';
+  static const String updateAddress = 'user/address';
+  static const String addresses = 'user/address';
 
   // User
-  static const String getUserProfile = "user/user";
-  static const String updateUserProfile = "user/update";
+  static const String getUserProfile = "user/user/user/user";
+  static const String updateUserProfile = "user/user/update";
 
   // Orders
-  static const String getOrders = "booking";
-  static const String createOrder = "booking/create";
-  static const String updateOrder = "/orders/update";
+  static const String getOrders = "user/booking";
+  static const String createOrder = "user/booking/create";
+  static const String updateOrder = "user//orders/update";
 
   //SETTINGS
-  static const String notifications = "notification";
-  static const String settings = "setting";
-  static const String slots = "slot";
-  static const String additionalInfo = "additional-info";
-  static const String contactUs = "contact/create";
-  static const String privacyPolicy = "content/privacy-policy";
-  static const String termsAndConditions = "content/terms-conditions";
-  static const String deleteAccount = "user/delete-account";
-  static const String logout = "user/logout";
+  static const String notifications = "user/notification";
+  static const String settings = "user/setting";
+  static const String slots = "user/slot";
+  static const String additionalInfo = "user/additional-info";
+  static const String contactUs = "user/contact/create";
+  static const String privacyPolicy = "user/content/privacy-policy";
+  static const String termsAndConditions = "user/content/terms-conditions";
+  static const String deleteAccount = "user/user/delete-account";
+  static const String logout = "user/user/logout";
 }
