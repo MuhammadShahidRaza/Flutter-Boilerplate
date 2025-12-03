f56dbd86dc185e79a835a43df82256664abdee09
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-12-03 23:48:57 +0500

    Update UserModel: Make firstName, lastName, and customerId nullable and enhance JSON parsing with default values

--- lib/data/models/user.dart
+++ lib/data/models/user.dart
@@ -1,7 +1,9 @@
+import 'package:sanam_laundry/providers/index.dart';
+
 class UserModel {
   final int id;
-  final String firstName;
-  final String lastName;
+  final String? firstName;
+  final String? lastName;
   final String email;
   final String phone;
   final String? providerId;
@@ -10,7 +12,7 @@ class UserModel {
   final String? bio;
   final String userType;
   final String? userRole;
-  final String customerId;
+  final String? customerId;
   final String? profileImage;
   final String? gender;
   final int? isRiderActive;
@@ -31,7 +33,7 @@ class UserModel {
     this.bio,
     required this.userType,
     this.userRole,
-    required this.customerId,
+    this.customerId,
     this.profileImage,
     this.gender,
     this.isRiderActive,
@@ -44,27 +46,27 @@ class UserModel {
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
       id: json['id'],
-      firstName: json['first_name'],
-      lastName: json['last_name'],
-      email: json['email'],
-      phone: json['phone'],
-      providerId: json['provider_id'],
+      firstName: json['first_name'] ?? "",
+      lastName: json['last_name'] ?? "",
+      email: json['email'] ?? "",
+      phone: json['phone'] ?? "",
+      providerId: json['provider_id']?.toString(),
       provider: json['provider'],
       countryCode: json['country_code'],
       bio: json['bio'],
-      userType: json['user_type'],
+      userType: json['user_type'] ?? "",
       userRole: json['user_role'],
-      customerId: json['customer_id'],
+
+      customerId: json['customer_id'] ?? "",
       profileImage: json['profile_image'],
       gender: json['gender'],
       isRiderActive: json['is_rider_active'],
       status: json['status'],
-      language: json['language'],
+      language: json['language'] ?? Language.en.name,
       createdAt: json['created_at'],
       token: json['token'],
     );
   }
-
   Map<String, dynamic> toJson() {
     return {
       'id': id,
