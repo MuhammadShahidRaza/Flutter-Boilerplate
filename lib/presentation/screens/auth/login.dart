99d1302d9424186ad99ad06b6a7b40cb4cf9adae
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-12-03 23:49:30 +0500

    Refactor Login screen: Enhance user authentication flow for riders and update input handling for email and password

--- lib/presentation/screens/auth/login.dart
+++ lib/presentation/screens/auth/login.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sanam_laundry/core/index.dart';
 import 'package:sanam_laundry/data/index.dart';
 import 'package:sanam_laundry/presentation/index.dart';
+import 'package:sanam_laundry/providers/index.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -12,14 +15,21 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final AuthRepository _authRepository = AuthRepository();
+  final RiderRepository _riderRepository = RiderRepository();
   final _formKey = GlobalKey<FormState>();
   final phoneController = TextEditingController();
+  final emailController = TextEditingController(
+    text: kDebugMode ? "harley58@example.com" : "",
+  );
+  final passwordController = TextEditingController(
+    text: kDebugMode ? "Qwerty@123" : "",
+  );
   bool loading = false;
   Future<void> _submit() async {
     setState(() => loading = true);
-    final message = await _authRepository.login(
-      phone: phoneController.text.trim(),
-    );
+    final message = await _authRepository.login({
+      'phone': phoneController.text.trim(),
+    });
     if (!mounted) return;
     if (message != null) {
       if (message == "loginSuccessful") {
@@ -44,24 +54,73 @@ class _LoginState extends State<Login> {
     setState(() => loading = false);
   }
 
+  Future<void> _submitRider() async {
+    setState(() => loading = true);
+    final user = await _riderRepository.login({
+      'email': emailController.text.trim(),
+      'password': passwordController.text.trim(),
+    });
+    if (!mounted) return;
+    if (user != null) {
+      context.read<AuthProvider>().login(context, user);
+      AppDialog.show(
+        context,
+        title: Common.successfullyLoggedIn,
+        imagePath: AppAssets.rightTick,
+        spacing: Dimens.spacingMSmall,
+        dismissible: false,
+        content: AppText(
+          maxLines: 3,
+          textAlign: TextAlign.center,
+          Auth.gladToHadYouBack,
+        ),
+        crossAxisAlignment: CrossAxisAlignment.center,
+        insetPadding: EdgeInsets.all(Dimens.spacingXXL),
+      );
+      Future.delayed(const Duration(seconds: 3), () {
+        if (!mounted) return;
+        context.replacePage(AppRoutes.riderHome);
+      });
+    }
+    setState(() => loading = false);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isRider = context.read<UserProvider>().isRider;
+
     return AuthWrapper(
       formKey: _formKey,
-      height: true,
       title: Auth.welcomeBackLogin,
       subtitle: Auth.helloAgainLogin,
       isLoading: loading,
       buttonText: Common.signIn,
-      onSubmit: _submit,
+      onSubmit: isRider ? _submitRider : _submit,
       child: Column(
+        spacing: Dimens.spacingS,
         children: [
-          AppPhoneInput(
-            title: Common.phoneNumber,
-            hint: Common.enterPhoneNumber,
-            controller: phoneController,
-            marginBottom: Dimens.spacingM,
-          ),
+          if (isRider) ...[
+            AppInput(
+              title: Common.email,
+              hint: Common.enterYourEmail,
+              controller: emailController,
+              fieldKey: FieldType.email,
+            ),
+            AppInput(
+              title: Common.password,
+              hint: Common.enterYourPassword,
+              controller: passwordController,
+              fieldKey: FieldType.password,
+              obscureText: true,
+            ),
+          ] else ...[
+            AppPhoneInput(
+              title: Common.phoneNumber,
+              hint: Common.enterPhoneNumber,
+              controller: phoneController,
+              marginBottom: Dimens.spacingM,
+            ),
+          ],
         ],
       ),
     );
