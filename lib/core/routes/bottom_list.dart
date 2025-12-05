6941c2ac7f4e825743026d7a4b0400628520eae1
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-12-06 01:31:27 +0500

    Refactor getBottomNavItems: Add rider-specific navigation items and improve structure

--- lib/core/routes/bottom_list.dart
+++ lib/core/routes/bottom_list.dart
@@ -2,27 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:sanam_laundry/core/index.dart';
 
 /// Returns translated bottom navigation items.
-List<BottomNavigationBarItem> getBottomNavItems(BuildContext context) {
-  return [
-    BottomNavigationBarItem(
-      icon: const Icon(Icons.home_outlined),
-      activeIcon: const Icon(Icons.home),
-      label: context.tr(Common.home),
-    ),
-    BottomNavigationBarItem(
-      icon: const Icon(Icons.local_laundry_service_outlined),
-      activeIcon: const Icon(Icons.local_laundry_service),
-      label: context.tr(Common.service),
-    ),
-    BottomNavigationBarItem(
-      icon: const Icon(Icons.assignment_outlined),
-      activeIcon: const Icon(Icons.assignment),
-      label: context.tr(Common.myOrders),
-    ),
-    BottomNavigationBarItem(
-      icon: const Icon(Icons.person_outline),
-      activeIcon: const Icon(Icons.person),
-      label: context.tr(Common.myAccount),
-    ),
-  ];
+List<BottomNavigationBarItem> getBottomNavItems(
+  BuildContext context,
+  bool isRider,
+) {
+  return isRider
+      ? [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.home_outlined),
+            activeIcon: const Icon(Icons.home),
+            label: context.tr(Common.home),
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.directions_car_outlined),
+            activeIcon: const Icon(Icons.directions_car),
+            label: context.tr(Common.myJobs),
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.person_outline),
+            activeIcon: const Icon(Icons.person),
+            label: context.tr(Common.myAccount),
+          ),
+        ]
+      : [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.home_outlined),
+            activeIcon: const Icon(Icons.home),
+            label: context.tr(Common.home),
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.local_laundry_service_outlined),
+            activeIcon: const Icon(Icons.local_laundry_service),
+            label: context.tr(Common.service),
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.assignment_outlined),
+            activeIcon: const Icon(Icons.assignment),
+            label: context.tr(Common.myOrders),
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.person_outline),
+            activeIcon: const Icon(Icons.person),
+            label: context.tr(Common.myAccount),
+          ),
+        ];
 }
