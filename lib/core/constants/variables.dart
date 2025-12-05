9e726f1b133b3f2b2a8241c41d154ff7d21913f5
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-12-06 01:31:49 +0500

    Add userRole constant to Variables for user role management

--- lib/core/constants/variables.dart
+++ lib/core/constants/variables.dart
@@ -2,5 +2,6 @@ class Variables {
   static const String userToken = "userToken";
   static const String hasVisitedApp = "hasVisitedApp";
   static const String userData = "userData";
+  static const String userRole = "userRole";
   static const String languageCode = "languageCode";
 }
