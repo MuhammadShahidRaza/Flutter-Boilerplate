97bce02d0baaab07c98efb9bdc54333cfa2d102d
Muhammad Shahid Raza <muhammadshahidraza8@gmail.com>
2025-12-09 02:09:14 +0500

    Add Google Maps API key to AppDelegate for map functionality

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("")
+    GMSServices.provideAPIKey("AIzaSyApndMQavta3YWmiNeVdfEX9oweWDn3hXg")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
